ea28d39bfe5249b0171919bf6e41c4494227354d
Hui Shan Pan <shan.pan@hotmail.com>
2024-08-20 23:07:54 +1000

    pt 1 complete

--- folders/folders.go
+++ folders/folders.go
@@ -4,19 +4,78 @@ import (
 	"github.com/gofrs/uuid"
 )
 
+
+/**
+New Suggested Function
+Retrieve all folders associated with a specific `OrgID` by:
+	1. fetching a list of filtered folder pointers from source using `FetchAllFoldersByOrgID` function
+	2. copy the filtered folder pointers into a new slice `fp`
+	3. creating and returning a `FetchFolderResponse` containing these folder pointers
+
+Improvments:
+	- Removed redundant loops that converts pointers->values->pointers, now function directly uses
+	  `r` retrieved from `FetchAllFoldersByOrgID` for `fp`
+	- Error handles potential error from `FetchAllFoldersByOrgID`, function will now immediately
+	  return `nil` and the error
+	- Unused variables from the previous `GetAllFolders` (`err`,`f1`,`fs`,`k`,`k1`) have been removed
+*/
 func GetAllFolders(req *FetchFolderRequest) (*FetchFolderResponse, error) {
-	var (
-		err error
-		f1  Folder
-		fs  []*Folder
-	)
+	//fetch folders by OrgID
+	r, err := FetchAllFoldersByOrgID(req.OrgID)
+	if err != nil {
+		return nil, err
+	}
+
+	// copy into folders
+	var fp []*Folder
+	for _, v1 := range r {
+		fp = append(fp, v1)
+	}
+
+	// create FetchFolderResponse with folder pointers
+	ffr := &FetchFolderResponse{Folders: fp}
+	return ffr, nil
+}
+
+
+/**
+Original Function
+Retrieve all folders associated with a specific `OrgID` by:
+	1. fetching a list of filtered folder pointers from source using `FetchAllFoldersByOrgID` function
+	2. convert these pointers to folder structs and store them in a temp variable `f`
+	3. convert folder values in `f`` back into pointers, storing them in `fp`
+	4. package these folder pointers `fp` into `FetchFolderResponse` struct and return this response
+
+Deprecated: due to redudant loops from pointers -> folder structs -> values -> pointers
+see improvement comments below + suggested improved function above
+
+*NOTE: To compile, some lines were modified (these are marked with CHANGED)*
+*/
+func DeprecatedGetAllFolders(req *FetchFolderRequest) (*FetchFolderResponse, error) {
+	// err,f1,fs declared but not not used, can be removed (compiler provides error messages)
+	// CHANGED: comment lines 57-61 out to compile
+	// var (
+	// 	err error
+	// 	f1  Folder
+	// 	fs  []*Folder
+	// )
+
+	// f unnecessary, used temporarily to hold values that are later converted back to pointers
 	f := []Folder{}
-	r, _ := FetchAllFoldersByOrgID(req.OrgID)
-	for k, v := range r {
+	r, _ := FetchAllFoldersByOrgID(req.OrgID) // filter folders with `orgID` but ignores error
+
+	// loop not necessary- no need to convert pointers to values
+	// CHANGED: replace k with _ on line 69 to compile (k never used)
+	for _, v := range r {
 		f = append(f, *v)
 	}
+
+	// should use r to directly append to fp
 	var fp []*Folder
-	for k1, v1 := range f {
+
+	//loop redundant, converts values in `f` back into pointers (original `r` contains all pointers)
+	// CHANGED: replace k1 with _ on line 78 to compile (k1 never used)
+	for _, v1 := range f {
 		fp = append(fp, &v1)
 	}
 	var ffr *FetchFolderResponse
@@ -24,6 +83,10 @@ func GetAllFolders(req *FetchFolderRequest) (*FetchFolderResponse, error) {
 	return ffr, nil
 }
 
+/**
+Retrieves all folders associated with a given `OrgID`. Filters through a sample Dataset
+and returns pointers to the folders that match `OrgID`
+*/
 func FetchAllFoldersByOrgID(orgID uuid.UUID) ([]*Folder, error) {
 	folders := GetSampleData()
 
--- folders/folders_test.go
+++ folders/folders_test.go
@@ -2,12 +2,76 @@ package folders_test
 
 import (
 	"testing"
-	// "github.com/georgechieng-sc/interns-2022/folders"
-	// "github.com/stretchr/testify/assert"
+	"github.com/georgechieng-sc/interns-2022/folders"
+	"github.com/gofrs/uuid"
+	"github.com/stretchr/testify/assert"
 )
 
 func Test_GetAllFolders(t *testing.T) {
-	t.Run("test", func(t *testing.T) {
-		// your test/s here
+	t.Run("Default and valid OrgID returns folders", func(t *testing.T) {
+		req := &folders.FetchFolderRequest{
+			OrgID: uuid.FromStringOrNil(folders.DefaultOrgID),
+		}
+
+		res, err := folders.GetAllFolders(req)
+
+		assert.NoError(t, err)
+		assert.NotNil(t, res)
+		assert.Greater(t, len(res.Folders), 0) // at least 1 folder
+
+		//check has valid id, name, OrgId
+		for _, folder := range res.Folders {
+			assert.NotEqual(t, uuid.Nil, folder.Id)
+			assert.NotEmpty(t, folder.Name)
+			assert.Equal(t, req.OrgID, folder.OrgId)
+		}
 	})
+
+	t.Run("Invalid OrgID returns NilOrgID + no folders", func(t *testing.T) {
+		req := &folders.FetchFolderRequest{
+			OrgID: uuid.FromStringOrNil("hello"),
+		}
+
+		res, err := folders.GetAllFolders(req)
+
+		assert.NoError(t, err)
+		assert.NotNil(t, res)
+		assert.Equal(t, 0, len(res.Folders)) //no folders
+		assert.Equal(t, uuid.Nil, req.OrgID) //OrgID should be nil bc invalid UUID input
+	})
+
+	t.Run("Non-existent OrgID returns no folders", func(t *testing.T) {
+		req := &folders.FetchFolderRequest{
+			OrgID: uuid.Must(uuid.NewV4()), //generate a new UUID
+		}
+
+		res, err := folders.GetAllFolders(req)
+
+		assert.NoError(t, err)
+		assert.NotNil(t, res)
+		assert.Equal(t, 0, len(res.Folders)) //no folder
+	})
+
+	t.Run("Nil OrgID returns no folders", func(t *testing.T) {
+		req := &folders.FetchFolderRequest{
+			OrgID: uuid.Nil, // use nil UUID
+		}
+
+		res, err := folders.GetAllFolders(req)
+
+		assert.NoError(t, err)
+		assert.NotNil(t, res)
+		assert.Equal(t, 0, len(res.Folders)) //no folder
+	})
+
+	t.Run("No OrgID provided returns no folders", func(t *testing.T) {
+        req := &folders.FetchFolderRequest{} //create req with no OrgID
+
+        res, err := folders.GetAllFolders(req)
+
+        assert.NoError(t, err)
+        assert.NotNil(t, res)
+        assert.Equal(t, 0, len(res.Folders))
+    })
+
 }
